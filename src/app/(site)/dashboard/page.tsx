f457339c8f2132b51801788b04732cdd31935265
kevin <47770053+kevinwchen@users.noreply.github.com>
2023-07-02 22:41:40 +1000

    Add dashboard page

---
+++ src/app/(site)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { SignInButton } from "@/components/SignInButton"
+import React from "react"
+
+export default function Dashboard() {
+  return (
+    <>
+      <SignInButton />
+      <div>Dashboard</div>
+    </>
+  )
+}
