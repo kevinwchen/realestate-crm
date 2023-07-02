88f7ee26753f15414f3bfdfdec7afb60b8f42b58
kevin <47770053+kevinwchen@users.noreply.github.com>
2023-07-02 22:43:09 +1000

    Update protected page route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware"
 
 export const config = {
-  matcher: ["/UserPost/:path*"], // protect all URLs starting with /UserPost
+  matcher: ["/dashboard/:path*"], // protect all URLs starting with /UserPost
 }
